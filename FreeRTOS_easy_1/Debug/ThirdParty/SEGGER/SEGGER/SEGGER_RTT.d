832025da0724cfeb0bf17bc000b49b0e8eca483c
Mehmetdora <mehmetdora333@gmail.com>
2026-07-03 15:08:22 +0300

    freeRTOS Easy 1

---
+++ FreeRTOS_easy_1/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
+ ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
+ ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ /Users/mehmet_dora/STM32CubeIDE/FreeRTOS_STM32/FreeRTOS_easy_1/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+/Users/mehmet_dora/STM32CubeIDE/FreeRTOS_STM32/FreeRTOS_easy_1/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
---
+++ FreeRTOS_easy_1/Drivers/Modules/GPIO_Driver.h
@@ -0,0 +1,20 @@
+/*
+ * GPIO_Driver.h
+ *
+ *  Created on: Jun 28, 2026
+ *      Author: mehmet_dora
+ */
+
+#ifndef MODULES_GPIO_DRIVER_H_
+#define MODULES_GPIO_DRIVER_H_
+
+
+#include "stdint.h"
+
+
+void gpio_setup(void);
+void gpio_set(uint8_t pin_num);
+void gpio_reset(uint8_t pin_num);
+
+
+#endif /* MODULES_GPIO_DRIVER_H_ */
